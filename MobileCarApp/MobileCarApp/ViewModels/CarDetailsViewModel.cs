bf4daf75b8a958766b604efbe33d5b62fc77ec69
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-08-07 08:54:15 +0200

    Small fixes to update/show the list

--- MobileCarApp/MobileCarApp/ViewModels/CarDetailsViewModel.cs
+++ MobileCarApp/MobileCarApp/ViewModels/CarDetailsViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MobileCarApp.Models;
 using MobileCarApp.ViewModels.Common;
+using System.Web;
 
 namespace MobileCarApp.ViewModels;
 
-[QueryProperty(nameof(Car), "Car")]
-public partial class CarDetailsViewModel : BaseViewModel
+[QueryProperty(nameof(Id), nameof(Id))]
+public partial class CarDetailsViewModel : BaseViewModel, IQueryAttributable
 {
     [ObservableProperty]
     Car car;
+    [ObservableProperty]
+    int id;
 
     public CarDetailsViewModel()
     {
+    }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        Id = Convert.ToInt32(HttpUtility.UrlDecode(query[nameof(Id)].ToString()));
+        Car = App.CarServices.GetCar(Id);
     }
 }
--- MobileCarApp/MobileCarApp/ViewModels/CarListViewModel.cs
+++ MobileCarApp/MobileCarApp/ViewModels/CarListViewModel.cs
@@ -1,25 +1,36 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MobileCarApp.Models;
-using MobileCarApp.Services;
 using MobileCarApp.ViewModels.Common;
 using MobileCarApp.Views;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Text.Json;
 
 namespace MobileCarApp.ViewModels;
 
 public partial class CarListViewModel : BaseViewModel
 {
+    const string editButtonText = "Update Car";
+    const string createButtonText = "Add Car";
     public ObservableCollection<Car> Cars { get; private set; } = new();
 
     [ObservableProperty]
     bool isRefreshing;
+    [ObservableProperty]
+    string make;
+    [ObservableProperty]
+    string model;
+    [ObservableProperty]
+    string vin;
+    [ObservableProperty]
+    string addEditButtonText;
+    [ObservableProperty]
+    int carId;
 
     public CarListViewModel()
     {
         Title = "Car List";
+        AddEditButtonText = createButtonText;
         GetCarList().Wait();
     }
 
@@ -51,13 +62,84 @@ public partial class CarListViewModel : BaseViewModel
     }
 
     [RelayCommand]
-    async Task GetCarDetails(Car car)
+    async Task GetCarDetails(int id)
     {
-        if (car == null) return;
+        if (id == 0) return;
 
-        await Shell.Current.GoToAsync(nameof(CarDetailsPage), true, new Dictionary<string, object>
+        await Shell.Current.GoToAsync($"{nameof(CarDetailsPage)}?Id={id}", true);
+    }
+
+    [RelayCommand]
+    async Task SaveCar()
+    {
+        if (string.IsNullOrEmpty(Model) || string.IsNullOrEmpty(Make) || string.IsNullOrEmpty(Vin))
         {
-            {nameof(Car), car}
-        });
+            await Shell.Current.DisplayAlert("Invalid Data", "Please insert valid data and try again", "OK");
+            return;
+        };
+
+        var car = new Car { Make = Make, Model = Model, Vin = Vin };
+
+        if(CarId != 0)
+        {
+            car.Id = CarId;
+            App.CarServices.UpdateCar(car);
+            await Shell.Current.DisplayAlert("Info", App.CarServices.StatusMessage, "Ok");
+        }
+        else
+        {
+            App.CarServices.AddCar(car);
+            await Shell.Current.DisplayAlert("Info", App.CarServices.StatusMessage, "Ok");
+        }
+        await GetCarList();
+        await ClearForm();
+    }
+
+    [RelayCommand]
+    async Task UpdateCar(int id)
+    {
+        AddEditButtonText = editButtonText;
+        return;
+    }
+
+    [RelayCommand]
+    async Task DeleteCar(int id)
+    {
+        if(id == 0)
+        {
+            await Shell.Current.DisplayAlert("Invalid Record", "Please try again", "OK");
+            return;
+        }
+        var result = App.CarServices.DeleteCar(id);
+        if(result == 0)
+        {
+            await Shell.Current.DisplayAlert("Failed", App.CarServices.StatusMessage, "OK");
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Deletion Successful", App.CarServices.StatusMessage, "OK");
+            await GetCarList();
+        }
+    }
+
+    [RelayCommand]
+    async Task SetEditMode(int id)
+    {
+        AddEditButtonText = editButtonText;
+        CarId = id;
+        var car = App.CarServices.GetCar(id);
+        Make = car.Make;
+        Model = car.Model;
+        Vin = car.Vin;
+    }
+
+    [RelayCommand]
+    async Task ClearForm()
+    {
+        AddEditButtonText = createButtonText;
+        CarId = 0;
+        Make = string.Empty;
+        Model = string.Empty;
+        Vin = string.Empty;
     }
 }
